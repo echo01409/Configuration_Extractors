1032b95a5857a5199c65cf5a9f6a3f607588fdf1
Echo <78220748+echo01409@users.noreply.github.com>
2024-11-23 23:32:26 +0000

    Update hex-array2key.py

--- hex-array2key.py
+++ hex-array2key.py
@@ -20,5 +20,5 @@ iv_hex_values = re.findall(r'0x([0-9A-Fa-f]{2})',iv_data)
 key_hex_string = "".join(key_hex_values)
 iv_hex_string = "".join(iv_hex_values)
 
-print("[+] Key:", key_hex_string)
-print("[+] IV:", iv_hex_string)
+print("[-] Key:", key_hex_string)
+print("[-] IV:", iv_hex_string)
